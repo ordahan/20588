8178556d865b59b761f4317c62a2600f3ce3084e
ord <mailordahan@gmail.com>
2014-01-12 15:45:19 +0200

    Split the test cases better

--- rtsp/src/rtsp/tests/test_message.py
+++ rtsp/src/rtsp/tests/test_message.py
@@ -22,7 +22,9 @@ class TestMessage(unittest.TestCase):
         self.assertEqual('OPTIONS\n\rCSeq: 13',
                          str(request_generated))
 
-    def testOptions(self):
+class TestOptions(TestMessage):
+
+    def testNominalParse(self):
         '''
         Tests the parsing of the options message
         '''
@@ -45,8 +47,9 @@ class TestMessage(unittest.TestCase):
         self.assertEqual(directives.OPTIONS, self.request.directive)
 
         # TODO: More validity tests for parsing
+        # 1) Options without URI - atm failing
 
-    def testOptionsResponse(self):
+    def testNominalResponse(self):
         response = \
             '\r\n'.join(["RTSP/1.0 200 OK",
                          "CSeq: 2",
@@ -56,6 +59,25 @@ class TestMessage(unittest.TestCase):
         self.assertEqual(response,
                          str(OptionsResponseMessage(sequence=2,
                                                     result=results.OK)))
+
+class TestDescribe(TestMessage):
+
+    def testDescribe(self):
+        '''
+        Tests the parsing of the describe message
+        '''
+        # Basic valid message
+        message = \
+            '\r\n'.join(['Sending request: DESCRIBE rtsp://localhost:8554/homeland.avi RTSP/1.0',
+                         'CSeq: 3',
+                         'User-Agent: LibVLC/2.0.8 (LIVE555 Streaming Media v2011.12.23)',
+                         'Accept: application/sdp',
+                         '\r\n'])
+
+        self.request.parse(message)
+
+        self.assertEqual(2, self.request.sequence)
+        self.assertEqual(directives.OPTIONS, self.request.directive)
 
 
 if __name__ == "__main__":

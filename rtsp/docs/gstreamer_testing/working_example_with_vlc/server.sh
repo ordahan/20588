752324b7964ff807b4ac2efa3aedb403177f851d
ord <mailordahan@gmail.com>
2014-03-02 12:42:21 +0200

    * Both video and audio works great in our example now :)

--- rtsp/docs/gstreamer_testing/working_example_with_vlc/server.sh
+++ rtsp/docs/gstreamer_testing/working_example_with_vlc/server.sh
@@ -1,2 +1,10 @@
-gst-launch-0.10 -v gstrtpbin name=rtpbin1 filesrc location=/home/ord/git/rtsp/rtsp/docs/30rock.avi ! decodebin ! x264enc ! rtph264pay ! rtpbin1.send_rtp_sink_0 rtpbin1.send_rtp_src_0 ! udpsink host=127.0.0.1 port=5004 rtpbin1.send_rtcp_src_0 ! udpsink host=127.0.0.1 port=5012 udpsrc port=5013 ! rtpbin1.recv_rtcp_sink_0 
-
+gst-launch-0.10 -v gstrtpbin name=rtpbin1 \
+filesrc location=/home/ord/git/rtsp/rtsp/docs/30rock.avi ! decodebin name=dec \
+dec.  ! queue ! x264enc ! rtph264pay ! rtpbin1.send_rtp_sink_0 \
+rtpbin1.send_rtp_src_0 ! udpsink host=127.0.0.1 port=5004 \
+rtpbin1.send_rtcp_src_0 ! udpsink host=127.0.0.1 port=5005 \
+udpsrc port=5009 ! rtpbin1.recv_rtcp_sink_0 \
+dec. ! queue ! audioresample ! audioconvert ! alawenc ! rtppcmapay ! rtpbin1.send_rtp_sink_1 \
+rtpbin1.send_rtp_src_1 ! udpsink host=127.0.0.1 port=5006 \
+rtpbin1.send_rtcp_src_1 ! udpsink host=127.0.0.1 port=5007 \
+udpsrc port=5011 ! rtpbin1.recv_rtcp_sink_1

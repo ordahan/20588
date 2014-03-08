b16060c06784d4be6d68b8f84fb7fcdc50748a90
ord <mailordahan@gmail.com>
2014-03-08 17:46:07 +0200

    Tested and fixed parsing of GET_PARAMETER request

--- rtsp/src/rtsp/tests/test_message.py
+++ rtsp/src/rtsp/tests/test_message.py
@@ -193,6 +193,47 @@ class TestSetup(TestMessage):
 
         self.assertMessagesEqual(expected_response, actual_response)
 
+# FIXME: Test PLAY message
+
+class TestGetParameter(TestMessage):
+
+    def testParse(self):
+        '''
+        Tests the parsing of the GET_PARAMETER message
+        '''
+        # Basic valid message
+        message = \
+            '\r\n'.join(['GET_PARAMETER rtsp://127.0.0.1:18554/homeland.avi RTSP/1.0',
+                         'CSeq: 7',
+                         'User-Agent: LibVLC/2.0.8 (LIVE555 Streaming Media v2011.12.23)',
+                         'Session: 18d000fccc6ddd5a',
+                         '\r\n'])
+
+        self.request.parse(message)
+
+        self.assertEqual(7, self.request.sequence)
+        self.assertEqual(directives.GET_PARAMETER, self.request.directive)
+
+#     def testResponse(self):
+#
+#         expected_response = \
+#             '\r\n'.join(['RTSP/1.0 200 OK',
+#                          'CSeq: 4',
+#                          'Content-Length: 0',
+#                          'Transport: RTP/AVP/UDP;unicast;client_port=52656-52657;server_port=30000-30001',
+#                          # FIXME: Generate a session for the message..not hardcode it
+#                          'Session: 12345',
+#                          '\r\n'])
+#
+#         actual_response = str(SetupResponseMessage(sequence=4,
+#                                                       result=result_codes.OK,
+#                                                       client_rtp_port=52656,
+#                                                       client_rtcp_port=52657,
+#                                                       server_rtp_port=30000,
+#                                                       server_rtcp_port=30001))
+#
+#         self.assertMessagesEqual(expected_response, actual_response)
+
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'TestMessage.testOptions']
     unittest.main()
